9be88efcd2b9b85fce0909bdb3a2423450318a56
diazk <diazk@uwplatt.edu>
2022-03-21 00:54:21 -0500

    Created header component @26:00

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,11 +1,11 @@
-
+import Header from './components/Header'
 function App() {
   const name = 'Korey'
   const x = false
 
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
     </div>
 
   );
