b52a8ac52e35ef7363052bef93ee8db775f6d619
diazk <diazk@uwplatt.edu>
2022-03-21 00:46:31 -0500

    Initial Push

---
+++ learning-react/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  const name = 'Korey'
+  const x = false
+
+  return (
+    <div className="container">
+      <h1>Hello From React</h1>
+    </div>
+
+  );
+}
+
+export default App;
