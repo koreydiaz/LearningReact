463365331b9851e900ba1846828fb39b8bc4cc5c
diazk <diazk@uwplatt.edu>
2022-03-22 23:56:34 -0500

    Added more components and created a basic task manager @1:14:35

--- learning-react/src/components/Header.js
+++ learning-react/src/components/Header.js
@@ -1,9 +1,31 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({ title }) => {
   return (
-      <header>
-          <h1>Learning React</h1>
+      <header className='header'>
+          <h1>{title}</h1>
+          <Button color='red' text='Add'/>
+
+
       </header>
   )
 }
+
+Header.defaultProps =
+{
+  title: 'React Webpage',
+} 
+
+Header.propTypes = {
+  title: PropTypes.string,
+}
+
+// CSS in JavaScript
+// const headingStyle = 
+// {
+//   color: 'red', 
+//   backgroundColor: 'blue'
+// }
 
 export default Header
